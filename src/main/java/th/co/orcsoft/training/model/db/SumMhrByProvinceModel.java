bb70678a16a4fa1d47d011799ad6e343517e02f7
Monchai Angkapiyasiri <got_loki@hotmail.com>
2019-06-07 14:55:51 +0700

    ViewModel

---
+++ src/main/java/th/co/orcsoft/training/model/db/SumMhrByProvinceModel.java
@@ -0,0 +1,30 @@
+package th.co.orcsoft.training.model.db;
+
+public class SumMhrByProvinceModel {
+	
+	private int prvID;
+	private int ptyID;
+	private int mHR;
+	
+	public int getPrvID() {
+		return prvID;
+	}
+	public void setPrvID(int prvID) {
+		this.prvID = prvID;
+	}
+	public int getPtyID() {
+		return ptyID;
+	}
+	public void setPtyID(int ptyID) {
+		this.ptyID = ptyID;
+	}
+	public int getmHR() {
+		return mHR;
+	}
+	public void setmHR(int mHR) {
+		this.mHR = mHR;
+	}
+	
+	
+
+}
---
+++ src/main/java/th/co/orcsoft/training/model/db/SumTotalVote.java
@@ -0,0 +1,30 @@
+package th.co.orcsoft.training.model.db;
+
+public class SumTotalVote {
+	
+	private int ptyID;
+	private String ptyName;
+	private int totalVote;
+	
+	public int getPtyID() {
+		return ptyID;
+	}
+	public void setPtyID(int ptyID) {
+		this.ptyID = ptyID;
+	}
+	public String getPtyName() {
+		return ptyName;
+	}
+	public void setPtyName(String ptyName) {
+		this.ptyName = ptyName;
+	}
+	public int getTotalVote() {
+		return totalVote;
+	}
+	public void setTotalVote(int totalVote) {
+		this.totalVote = totalVote;
+	}
+	
+	
+	
+}
